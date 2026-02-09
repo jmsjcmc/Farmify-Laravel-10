d13b62453e6a4c6df4df0b5e735e28af43851240
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-02-09 09:44:16 +0800

    feat: create userfarm model, factory, migration, seeder, and controller

---
+++ database/seeders/UserFarmSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserFarmSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
