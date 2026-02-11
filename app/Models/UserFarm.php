f578b1e566b9f66956ac05f66051a13a6ae81dfd
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-02-11 11:41:03 +0800

    feat: modified user farm table

--- app/Models/UserFarm.php
+++ app/Models/UserFarm.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class UserFarm extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        ''
+    ]
 }
--- database/migrations/2026_02_09_014331_create_user_farms_table.php
+++ database/migrations/2026_02_09_014331_create_user_farms_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('user_farms', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('user_id')
+            ->constrained()
+            ->cascadeOnDelete();
+
+            $table->foreignId('farm_id')
+            ->constrained()
+            ->cascadeOnDelete();
+
+            $table->timestamp('created_at');
         });
     }
 
