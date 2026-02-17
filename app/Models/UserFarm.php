18f185ee67bf9bc178b50589f565a2b94ff265ee
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-02-18 07:52:34 +0800

    update: add user farm model

--- app/Models/UserFarm.php
+++ app/Models/UserFarm.php
@@ -8,7 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class UserFarm extends Model
 {
     use HasFactory;
+
     protected $fillable = [
-        ''
-    ]
+        'user_id',
+        'farm_id'
+    ];
 }
